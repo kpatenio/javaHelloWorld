dc63cdf8ea5d308d8bd8eda986035036c38bbc56
kpatenio <patenio@ualberta.ca>
2020-05-13 14:26:11 -0700

    added new function to branch1

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,4 +2,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HELLO WORLD"); // change in branch1
     }
+
+    public void foo() {
+        System.out.println("Foo");
+    }
 }
